999abff2600ed06b033bef5c1ff73b00e64874ab
asrajavel <asrajavel267@gmail.com>
2025-05-10 15:20:18 +0530

    Limit displayed mutual funds to the first 10 in the dropdown component

--- src/components/MutualFundDropdown.tsx
+++ src/components/MutualFundDropdown.tsx
@@ -7,13 +7,16 @@ interface MutualFundDropdownProps {
 }
 
 export const MutualFundDropdown: React.FC<MutualFundDropdownProps> = ({ funds, onSelect }) => {
+  // Take only first 10 funds
+  const limitedFunds = funds.slice(0, 10);
+
   return (
     <select 
       style={{ width: '100%', padding: '8px' }}
       onChange={(e) => onSelect(Number(e.target.value))}
     >
       <option value="">Select a mutual fund</option>
-      {funds.map(fund => (
+      {limitedFunds.map(fund => (
         <option key={fund.schemeCode} value={fund.schemeCode}>
           {fund.schemeName}
         </option>
